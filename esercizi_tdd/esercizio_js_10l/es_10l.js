c25e3449b62d6c1b3088e8a036d102448f704cd9
MarcoCellini <marco.cellini@istitutoagnelli.it>
2024-04-20 21:04:27 +0200

    cellini: fix es 10l for issue 15

--- esercizi_tdd/esercizio_js_10l/es_10l.js
+++ esercizi_tdd/esercizio_js_10l/es_10l.js
@@ -1,9 +1,3 @@
 export const special_concat = (s1, s2) => {
-    return s1 && s2 && s1.split(' ').length == 1 && s2.split(' ').length == 1 ?
-        `${s2.substring(0, 2)}${s1.substring(2,)}${s1.substring(0, 2)}${s2.substring(2,)}` :
-        'parole non adatte';
-        
+    return 0;        
 };
-/* 
-let str1 = '', str2 = 'Rossi fiippo';
-console.log(special_concat(str1, str2)); */
