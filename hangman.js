31487f78b2e2282d9a959b98fddecfdda849c301
Marco-Tranaso <marco.tranaso@edu-its.it>
2025-03-17 17:42:58 +0100

    aggiunta esercizio impiccato

--- hangman.js
+++ hangman.js
@@ -1,3 +1,33 @@
+/*
+Word Guesser
+
+● Create two arrays:
+  ○ one for the letters of the word (e.g. 'C', 'A', 'T')
+  ○ Another for the current guessed letters (start with '_', '_', '_' and add the correct letters to it).
+
+● Write a function called guessLetter that should:
+  ○ Take one parameter, a letter.
+  ○ Have a maximum number of guesses (e.g. 6).
+  ○ Check if the letter is in the word array.
+  ○ If the letter matches, add it in the correct position of the guessed array.
+  ○ Show the user the current guessed letters.
+  ○ Tell the user if they guessed a correct letter.
+  ○ Tell the user how many guesses remain.
+  ○ Tell the user if they won or lost the game.
+
+Call your function to make guesses:
+  guessLetter('G');
+  guessLetter('I');
+  guessLetter('O');
+  guessLetter('A');
+  guessLetter('T');
+
+● Bonus:
+  ○ Add a reward for correct guesses and subtract an amount for failed guesses.
+  ○ Show the user the total reward (positive or negative).
+*/
+
+
 let array1 = ['C', 'A', 'T'];
 let array2 = ['_', '_', '_'];
 let max_guess = 6;  
